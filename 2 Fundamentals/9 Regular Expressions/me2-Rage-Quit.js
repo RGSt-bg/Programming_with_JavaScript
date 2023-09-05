f1220b4bed4afd75477288b5aeb0c1b35f51b042
Rumen Stefanov <rgst.bg@gmail.com>
2023-09-05 16:17:58 +0300

    Add files via upload

---
+++ 2 Fundamentals/9 Regular Expressions/me2-Rage-Quit.js
@@ -0,0 +1,8 @@
+function rageQuit(inputStr) {
+
+    let pattern = /(?<str>[\D]{1,20})(?<num>[\d]{1,2})/g;
+}
+
+rageQuit("a3");
+console.log("---------------------------------");
+rageQuit("aSd2&5s@1");
